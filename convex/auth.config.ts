327add99a4b34cf097d8004b517388724e74b86f
Irvin Tancioco <irvin.tanc@gmail.com>
2024-11-01 19:35:17 -0700

    update usser.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,10 @@
-export default {
-    providers: [
-      {
-        domain: "https://tops-worm-73.clerk.accounts.dev",
-        applicationID: "convex",
-      },
-    ]
-  };
+const clerkConfig = {
+  providers: [
+    {
+      domain: "https://tops-worm-73.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default clerkConfig;
--- convex/https.ts
+++ convex/https.ts
@@ -2,7 +2,8 @@ import { httpRouter } from 'convex/server';
 import { httpAction } from './_generated/server';
 import { Webhook } from 'svix';
 import type { WebhookEvent } from '@clerk/nextjs/server';
-import { internal } from './_gnerated/api'
+import { internal } from './_generated/api';
+
 const http = httpRouter();
 
 http.route({
@@ -11,18 +12,28 @@ http.route({
     handler: httpAction(async (ctx, req) => {
         const body = await validateRequest(req);
         if (!body) {
-            return new Response('Unauthorized', { status: 401 })
+            return new Response('Unauthorized', { status: 401 });
         }
-        switch (body.type {
-            await ctx.runMutation( internal.functions.user.upsert, )
+
+        switch (body.type) {
             case 'user.created':
+                await ctx.runMutation(internal.functions.user.upsert, { 
+                    username: body.data.username!,
+                    image: body.data.image_url,
+                    clerkId: body.data.id,
+                });
                 break;
             case 'user.updated':
+
                 break;
             case 'user.deleted':
+
                 break;
+            default:
+                return new Response('Unrecognized event', { status: 400 });
         }
-        return new Response('OK', { status: 200 })
+
+        return new Response('OK', { status: 200 });
     }),
 });
 
@@ -30,22 +41,21 @@ const validateRequest = async (req: Request) => {
     const headers = req.headers;
     const text = await req.text();
 
-    const svix_id = headers.get('svix-id')
-    const svix_timestamp = headers.get('svix-timestamp')
-    const svix_signature = headers.get('svix-signature')
+    const svix_id = headers.get('svix-id');
+    const svix_timestamp = headers.get('svix-timestamp');
+    const svix_signature = headers.get('svix-signature');
 
     try {
-        const webhook = new Webhook( process.env.CLERK_WEBHOOK_SECRET! );
-        return webhook.verify( text, {
-            id: svix_id,
-            timestamp: svix_timestamp,
-            signature: svix_signature,
+        const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET!);
+        return webhook.verify(text, {
+            id: svix_id!,
+            timestamp: svix_timestamp!,
+            signature: svix_signature!,
         }) as unknown as WebhookEvent;
     } catch (error) {
-        return error
-    };
-    
-
+        console.error('Webhook verification failed:', error);
+        return null;
+    }
 };
 
 export default http;
